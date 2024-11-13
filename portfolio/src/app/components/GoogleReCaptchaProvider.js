c46ba826d90b299c54865c9aa79a7028282114a4
IALTAIR <yferoze@gmail.com>
2024-11-13 23:21:05 +0100

    correction recaptcha

--- portfolio/src/app/components/GoogleReCaptchaProvider.js
+++ portfolio/src/app/components/GoogleReCaptchaProvider.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const GoogleReCaptchaProvider = ({ onVerify }) => {
     const [recaptchaLoaded, setRecaptchaLoaded] = useState(false);
