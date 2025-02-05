d2ab7d23a4e7a70685a257e510264bc3f10840c1
IALTAIR <yferoze@gmail.com>
2025-02-05 21:14:16 +0100

    correction reCaptcha V4

--- portfolio/src/app/components/GoogleReCaptchaProvider.js
+++ portfolio/src/app/components/GoogleReCaptchaProvider.js
@@ -1,45 +1,58 @@
 import React, { useEffect, useState } from "react";
 
 const GoogleReCaptchaProvider = ({ onVerify }) => {
-    const [recaptchaLoaded, setRecaptchaLoaded] = useState(false);
+  const [recaptchaLoaded, setRecaptchaLoaded] = useState(false);
 
-    useEffect(() => {
-        const loadReCAPTCHA = () => {
-            const script = document.createElement("script");
-            script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
-            script.onload = () => {
-                setRecaptchaLoaded(true);
-            };
-            document.body.appendChild(script);
+  useEffect(() => {
+    const loadReCAPTCHA = () => {
+      if (typeof window !== "undefined" && !window.grecaptcha) {
+        const script = document.createElement("script");
+        script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
+        script.onload = () => {
+          setRecaptchaLoaded(true);
         };
-
-        loadReCAPTCHA();
-    }, []);
-
-    const handleVerify = async () => {
-        if (recaptchaLoaded && window.grecaptcha) {
-            window.grecaptcha.ready(async () => {
-                try {
-                    const token = await window.grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY, { action: "submit" });
-                    onVerify(token);
-                } catch (error) {
-                    console.error("Erreur lors de l'exécution de reCAPTCHA :", error);
-                }
-            });
-        } else {
-            console.log("reCAPTCHA non chargé ou grecaptcha est indéfini");
-        }
+        document.body.appendChild(script);
+      }
     };
 
-    // Appel automatique de la vérification reCAPTCHA dès que le script est prêt
-    useEffect(() => {
-        if (recaptchaLoaded) {
-            handleVerify();
-        }
-    }, [recaptchaLoaded]);
+    loadReCAPTCHA();
+  }, []);
 
-    return null;
+  const handleVerify = async () => {
+    if (recaptchaLoaded && window.grecaptcha) {
+      window.grecaptcha.ready(async () => {
+        try {
+          const token = await window.grecaptcha.execute(
+            process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+            { action: "submit" }
+          );
+          if (onVerify && typeof onVerify === "function") {
+            onVerify(token);
+          } else {
+            console.warn(
+              "La fonction onVerify n'est pas définie ou n'est pas une fonction"
+            );
+          }
+        } catch (error) {
+          console.error("Erreur lors de l'exécution de reCAPTCHA :", error);
+        }
+      });
+    } else {
+      console.log("reCAPTCHA non chargé ou grecaptcha est indéfini");
+    }
+  };
+
+  // Appel automatique de la vérification reCAPTCHA dès que le script est prêt
+  useEffect(() => {
+    if (recaptchaLoaded) {
+      handleVerify();
+    }
+  }, [recaptchaLoaded]);
+
+  return null;
 };
 
+// Exporter handleVerify pour qu'il soit accessible ailleurs
 export { handleVerify };
+
 export default GoogleReCaptchaProvider;
