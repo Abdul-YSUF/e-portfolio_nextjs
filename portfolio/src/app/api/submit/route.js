0355a7e989955aaa51b12ee219e0a058a01a6748
IALTAIR <yferoze@gmail.com>
2025-02-05 14:56:42 +0100

    test recaptcha

--- portfolio/src/app/api/submit/route.js
+++ portfolio/src/app/api/submit/route.js
@@ -24,46 +24,54 @@ async function verifyRecaptcha(token) {
 
 // Fonction d'exportation pour la route API
 export async function POST(req) {
-  const { name, email, phone, message, recaptchaToken } = await req.json();
-
-  // Validez le reCAPTCHA ici
-  const isValidRecaptcha = await verifyRecaptcha(recaptchaToken);
-  if (!isValidRecaptcha) {
-    return new Response(JSON.stringify({ message: "reCAPTCHA non valide." }), {
-      status: 400,
-    });
-  }
-
-  // Configuration du transporteur d'e-mails
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_TO,
-    replyTo: email,
-    subject: `Nouveau message de ${name}`,
-    text: `Nom: ${name}\nEmail: ${email}\nTéléphone: ${phone}\nMessage:\n${message}`,
-  };
-
   try {
+    const body = await req.json();
+    console.log("Données reçues:", body); // Debugging
+
+    const { name, email, phone, message, recaptchaToken } = body;
+
+    if (!name || !email || !message || !recaptchaToken) {
+      return new Response(JSON.stringify({ message: "Données invalides." }), {
+        status: 400,
+      });
+    }
+
+    const isValidRecaptcha = await verifyRecaptcha(recaptchaToken);
+    if (!isValidRecaptcha) {
+      return new Response(
+        JSON.stringify({ message: "reCAPTCHA non valide." }),
+        { status: 400 }
+      );
+    }
+
+    // Config nodemailer
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT || 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_TO,
+      replyTo: email,
+      subject: `Nouveau message de ${name}`,
+      text: `Nom: ${name}\nEmail: ${email}\nTéléphone: ${phone}\nMessage:\n${message}`,
+    };
+
     await transporter.sendMail(mailOptions);
     return new Response(
       JSON.stringify({ message: "Email envoyé avec succès." }),
       { status: 200 }
     );
   } catch (error) {
-    console.error("Erreur lors de l'envoi de l'email:", error);
-    return new Response(
-      JSON.stringify({ message: "Erreur lors de l'envoi de l'email." }),
-      { status: 500 }
-    );
+    console.error("Erreur API:", error);
+    return new Response(JSON.stringify({ message: "Erreur serveur." }), {
+      status: 500,
+    });
   }
 }
