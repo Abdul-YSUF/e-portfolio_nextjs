5eafc1c2ecd1ac533aad3c4106a14cbf7a83deb2
IALTAIR <yferoze@gmail.com>
2025-02-08 15:16:38 +0100

    mineur modification scss

--- portfolio/src/app/components/Carrousel.js
+++ portfolio/src/app/components/Carrousel.js
@@ -188,9 +188,17 @@ const Popup = ({ project, onClose }) => {
           <div className="technologie-container">
             <h2 className="technologie-text">Technologies utilisées</h2>
             <div className="technologie-language">
-            {project.logos[0] && Object.values(project.logos[0]).map((logo, index) => (
-              <Image className="language-img" key={index} src={logo} alt="logo" width={20} height={20} />
-            ))}
+              {project.logos[0] &&
+                Object.values(project.logos[0]).map((logo, index) => (
+                  <Image
+                    className="language-img"
+                    key={index}
+                    src={logo}
+                    alt="logo"
+                    width={20}
+                    height={20}
+                  />
+                ))}
             </div>
           </div>
           <div className="demostrastion">
