a7fa1eb304a9b5e79376498e9d676241ed90635d
IALTAIR <yferoze@gmail.com>
2025-02-26 16:40:59 +0100

    correction erreurs grecaptcha et section services

--- portfolio/src/app/components/GoogleReCaptchaProvider.js
+++ portfolio/src/app/components/GoogleReCaptchaProvider.js
@@ -1,44 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 const GoogleReCaptchaProvider = ({ onVerify }) => {
-    const [recaptchaLoaded, setRecaptchaLoaded] = useState(false);
+  const [recaptchaLoaded, setRecaptchaLoaded] = useState(false);
 
-    useEffect(() => {
-        const loadReCAPTCHA = () => {
-            const script = document.createElement("script");
-            script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
-            script.onload = () => {
-                setRecaptchaLoaded(true);
-            };
-            document.body.appendChild(script);
-        };
-
-        loadReCAPTCHA();
-    }, []);
-
-    const handleVerify = async () => {
-        if (recaptchaLoaded && window.grecaptcha) {
-            window.grecaptcha.ready(async () => {
-                try {
-                    const token = await window.grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY, { action: "submit" });
-                    onVerify(token);
-                } catch (error) {
-                    console.error("Erreur lors de l'exécution de reCAPTCHA :", error);
-                }
-            });
-        } else {
-            console.log("reCAPTCHA non chargé ou grecaptcha est indéfini");
-        }
+  useEffect(() => {
+    const loadReCAPTCHA = () => {
+      const script = document.createElement("script");
+      script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
+      script.onload = () => {
+        setRecaptchaLoaded(true);
+      };
+      document.body.appendChild(script);
     };
 
-    // Appel automatique de la vérification reCAPTCHA dès que le script est prêt
-    useEffect(() => {
-        if (recaptchaLoaded) {
-            handleVerify();
-        }
-    }, [recaptchaLoaded]);
+    loadReCAPTCHA();
+  }, []);
 
-    return null;
+  const handleVerify = useCallback(async () => {
+    if (recaptchaLoaded && window.grecaptcha) {
+      window.grecaptcha.ready(async () => {
+        try {
+          const token = await window.grecaptcha.execute(
+            process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+            { action: "submit" }
+          );
+          onVerify(token);
+        } catch (error) {
+          console.error("Erreur lors de l'exécution de reCAPTCHA :", error);
+        }
+      });
+    } else {
+      console.log("reCAPTCHA non chargé ou grecaptcha est indéfini");
+    }
+  }, [recaptchaLoaded, onVerify]);
+
+  useEffect(() => {
+    if (recaptchaLoaded) {
+      handleVerify();
+    }
+  }, [recaptchaLoaded, handleVerify]);
+
+  return null;
 };
 
 export default GoogleReCaptchaProvider;
--- portfolio/src/app/components/Services.js
+++ portfolio/src/app/components/Services.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Image from "next/image";
 
 const services = [
-  { title: "DÉVELOPPEMENT WEB", imageService: "wds.webp", colorClass: "canva", description: "Développement front-end et back-end de sites web et gestion de CMS." },
-  //{ title: "UI/UX", colorClass: "ui-ux", description: "Design d'interfaces ergonomiques et intuitives." },
-  { title: "CONSEIL", imageService: "advice.webp", colorClass: "figma", description: "Prototypage rapide pour vos projets digitaux." },
-  //{ title: "FIGMA", colorClass: "figma", description: "Design collaboratif et création de maquettes." },
-  { title: "SEO et ACCESSIBILITÉ", imageService: "seoetacc.webp", colorClass: "wordpress", description: "Optimisation SEO et accessibilité pour améliorer la visibilité de votre site web." },
-  //{ title: "CMS", colorClass: "wordpress", description: "Développement de sites avec les CMS." },
-  //{ title: "CANVA", colorClass: "canva", description: "Création de visuels avec Canva." },
-  //{ title: "WEB DESIGNING", colorClass: "web-designing", description: "Design et conception de sites web." },
+  {
+    title: "DÉVELOPPEMENT WEB",
+    imageService: "wds.webp",
+    colorClass: "canva",
+    description:
+      "Développement front-end et back-end de sites web et gestion de CMS.",
+  },
+  {
+    title: "CONSEIL",
+    imageService: "advice.webp",
+    colorClass: "figma",
+    description: "Prototypage rapide pour vos projets digitaux.",
+  },
+  {
+    title: "SEO et ACCESSIBILITÉ",
+    imageService: "seoetacc.webp",
+    colorClass: "wordpress",
+    description:
+      "Optimisation SEO et accessibilité pour améliorer la visibilité de votre site web.",
+  },
 ];
 
 const Services = () => {
@@ -16,10 +29,22 @@ const Services = () => {
     <section id="services" className="services">
       <h2 className="stitre">Services</h2>
       <div className="services-container">
-        {services.map((service, index) => (
-          <div key={index} className={`service-card ${service.colorClass}`}>
+        {services.map((service) => (
+          <div
+            key={service.title}
+            className={`service-card ${service.colorClass}`}
+          >
             <h3>{service.title}</h3>
-            <img className="image-service" src={`/assets/${service.imageService}`} alt={service.title} />
+            {service.imageService && (
+              <Image
+                className="image-service"
+                src={`/assets/${service.imageService}`}
+                alt={`Illustration pour ${service.title}`}
+                width={150} // Ajuste la taille selon tes besoins
+                height={150}
+                priority
+              />
+            )}
             <p>{service.description}</p>
           </div>
         ))}
