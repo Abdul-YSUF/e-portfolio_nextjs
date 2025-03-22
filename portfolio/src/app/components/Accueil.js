647dd2e9ab839c92cf18bb86d8096e34c955d624
IALTAIR <yferoze@gmail.com>
2025-03-22 11:45:35 +0100

    modification header

--- portfolio/src/app/components/Accueil.js
+++ portfolio/src/app/components/Accueil.js
@@ -1,4 +1,94 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+const titles = [
+  "Développeur Web",
+  "Fullstack",
+  "Passionné du Code",
+  "Créateur d'Expériences Web",
+];
+
+export default function Accueil() {
+  const [title, setTitle] = useState(titles[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * titles.length);
+      setTitle(titles[randomIndex]);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="welcome">Bienvenue sur mon Portfolio</h1>
+      <h2>Abdul YSUF - {title}</h2>
+      <p className="description_welcome">
+        Je suis un développeur passionné par le web et la création d’expériences
+        uniques.
+      </p>
+      <div className="imageWrapper">
+        <Image
+          src="/assets/4962136_2597613.webp"
+          alt="Photo de profil"
+          width={280}
+          height={280}
+        />
+      </div>
+
+      <div className="barre_container">
+        <div className="barre">
+          <ul className="barre__reseau-social">
+            <li className="barre__vertical-content">
+              <a
+                href="https://github.com/Abdul-YSUF?tab=repositories"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="GitHub Abdul le Dev"
+                className="barre__logo-1"
+              >
+                <FaGithub />
+              </a>
+            </li>
+            <li className="barre__vertical-content">
+              <a
+                href="https://www.linkedin.com/in/abdul-fahad-ysuf-970887235/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LinkedIn Abdul le Dev"
+                className="barre__logo-2"
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+            <li className="barre__vertical-content">
+              <a
+                href="mailto:abdulledev@gmail.com"
+                aria-label="Envoyer un email à Abdul le Dev"
+                className="barre__logo-3"
+              >
+                <FaEnvelope />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <p className="contact_welcome">
+        Si vous souhaitez discuter d'un projet ou simplement échanger, n'hésitez
+        pas à me contacter !
+      </p>
+      <a href="#formulaire" aria-label="Aller à la section contact" className="contactButton">
+        Contactez-moi
+      </a>
+    </div>
+  );
+}
+
+/*import React from "react";
 import Image from "next/image";
 
 export default function Introduction() {
@@ -111,3 +201,4 @@ export default function Introduction() {
     </section>
   );
 }
+*/
