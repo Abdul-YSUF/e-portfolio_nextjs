e98c37e5ecb2d2683f6b45ce59a1648b2c1ed49e
IALTAIR <yferoze@gmail.com>
2025-03-23 19:00:56 +0100

    correction recaptcha

--- portfolio/src/app/components/Contact.js
+++ portfolio/src/app/components/Contact.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GoogleReCaptchaProvider from "./GoogleReCaptchaProvider";
 import Calendly from "./Calendly";
 
@@ -19,33 +19,64 @@ export default function ContactForm() {
     setRecaptchaToken(token);
   };
 
+  useEffect(() => {
+    if (!recaptchaToken) return;
+    console.log("reCAPTCHA token mis à jour :", recaptchaToken);
+  }, [recaptchaToken]);
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
     setErrors({ ...errors, [e.target.name]: "" });
   };
 
-  const validateForm = () => {
+  const handleBlur = (e) => {
+    const { name } = e.target;
+    validateForm(name);
+  };
+
+  const validateForm = (fieldToValidate = null) => {
     let valid = true;
-    let formErrors = {};
+    let formErrors = { ...errors };
 
-    if (!formData.name.trim()) {
-      formErrors.name = "⚠️ Veuillez entrer un nom et prénom valide.";
-      valid = false;
+    if (!fieldToValidate || fieldToValidate === "name") {
+      if (!/^[a-zA-ZÀ-ÿ\s'-]+$/.test(formData.name)) {
+        formErrors.name = "⚠️ Veuillez entrer un nom et prénom valide.";
+        valid = false;
+      } else {
+        formErrors.name = "";
+      }
     }
 
-    if (!formData.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-      formErrors.email = "⚠️ Veuillez entrer une adresse email valide.";
-      valid = false;
+    if (!fieldToValidate || fieldToValidate === "email") {
+      if (
+        !/^(?!.*\.\.)([a-zA-Z0-9._%+-]+)@[a-zA-Z0-9]+(\.[a-zA-Z0-9-]*)?\.[a-zA-Z]{2,}$/.test(
+          formData.email
+        )
+      ) {
+        formErrors.email = "⚠️ Veuillez entrer une adresse email valide.";
+        valid = false;
+      } else {
+        formErrors.email = "";
+      }
     }
 
-    if (!formData.phone.match(/^(?:\+33|0033|0)[1-9](\d{2}){4}$/)) {
-      formErrors.phone = "⚠️ Veuillez entrer un numéro de téléphone valide.";
-      valid = false;
+    if (!fieldToValidate || fieldToValidate === "phone") {
+      if (!/^(?:\+33|0033|0)[1-9](\d{2}){4}$/.test(formData.phone)) {
+        formErrors.phone =
+          "⚠️ Veuillez entrer un numéro de téléphone valide (formats acceptés : +33, 0033 ou 0).";
+        valid = false;
+      } else {
+        formErrors.phone = "";
+      }
     }
 
-    if (formData.message.trim().length < 10) {
-      formErrors.message = "⚠️ Veuillez entrer au moins 10 caractères.";
-      valid = false;
+    if (!fieldToValidate || fieldToValidate === "message") {
+      if (formData.message.trim().length < 10) {
+        formErrors.message = "⚠️ Veuillez entrer au moins 10 caractères.";
+        valid = false;
+      } else {
+        formErrors.message = "";
+      }
     }
 
     setErrors(formErrors);
@@ -55,13 +86,20 @@ export default function ContactForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!validateForm()) {
+    const isValid = validateForm();
+    if (!isValid) {
       setStatus("Veuillez corriger les erreurs dans le formulaire.");
+      setTimeout(() => {
+        setStatus("");
+      }, 3000);
       return;
     }
 
     if (!recaptchaToken) {
       setStatus("Validation reCAPTCHA en cours...");
+      setTimeout(() => {
+        setStatus("");
+      }, 3000);
       return;
     }
 
@@ -70,76 +108,175 @@ export default function ContactForm() {
     try {
       const response = await fetch("/api/submit", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ ...formData, recaptchaToken }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          recaptchaToken,
+        }),
       });
 
       const result = await response.json();
       if (response.ok) {
-        setStatus("Formulaire envoyé avec succès !");
-        setFormData({ name: "", email: "", phone: "", message: "" });
-        setRecaptchaToken(null);
+        handleSuccess();
       } else {
         setStatus(`Erreur : ${result.message}`);
+        setTimeout(() => {
+          setStatus("");
+        }, 3000);
       }
     } catch (error) {
       setStatus("Échec de la soumission. Veuillez réessayer.");
       console.error("Erreur de soumission :", error);
+      setTimeout(() => {
+        setStatus("");
+      }, 3000);
     }
 
     setIsSubmitting(false);
   };
 
+  const handleSuccess = () => {
+    setStatus("Votre formulaire a été soumis avec succès !");
+    setFormData({ name: "", email: "", phone: "", message: "" });
+    setRecaptchaToken(null);
+    setTimeout(() => {
+      setStatus("");
+    }, 3000);
+  };
+
   return (
-    <div className="formulaire_contact">
+    <div className="formulaire_contact" id="formulaire">
       <h2 className="stitre">Formulaire de contact</h2>
       <Calendly />
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Nom et prénom"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        {errors.name && <p className="error">{errors.name}</p>}
+      <div className="div_titre">
+        <p className="titre_form">
+          Sinon n'hésitez pas à me contacter pour toute question ou proposition
+          via le formulaire de contact ci-dessous
+        </p>
+      </div>
+      <div className="form_contact" id="contact">
+        <div className="divers">
+          <img
+            className="contact_img"
+            alt="contact"
+            src="./assets/image_form.webp"
+          />
+        </div>
+        <p className="titre_form-mobile">
+          Sinon n'hésitez pas à me contacter pour toute question ou proposition
+          par le formulaire de contact ci-dessous
+        </p>
+        <form className="form" onSubmit={handleSubmit}>
+          <label className="label_form" htmlFor="name">
+            Nom et Prénom
+          </label>
+          <input
+            id="name"
+            className={`nom_de_contact input_nuit ${
+              errors.name ? "input-error" : ""
+            }`}
+            type="text"
+            name="name"
+            autoComplete="name"
+            placeholder="Votre nom et prénom"
+            value={formData.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            required
+          />
+          {errors.name && (
+            <p className="error error-msg" role="alert">
+              {errors.name}
+            </p>
+          )}
 
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        {errors.email && <p className="error">{errors.email}</p>}
+          <label className="label_form" htmlFor="email">
+            Adresse email
+          </label>
+          <input
+            id="email"
+            className={`email input_nuit ${errors.email ? "input-error" : ""}`}
+            type="email"
+            name="email"
+            autoComplete="email"
+            placeholder="Votre adresse email"
+            value={formData.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            required
+          />
+          {errors.email && (
+            <p className="error error-msg" role="alert">
+              {errors.email}
+            </p>
+          )}
 
-        <input
-          type="text"
-          name="phone"
-          placeholder="Téléphone"
-          value={formData.phone}
-          onChange={handleChange}
-        />
-        {errors.phone && <p className="error">{errors.phone}</p>}
+          <label className="label_form" htmlFor="phone">
+            Numéro de Téléphone
+          </label>
+          <input
+            id="phone"
+            className={`telephone input_nuit ${
+              errors.phone ? "input-error" : ""
+            }`}
+            type="text"
+            name="phone"
+            autoComplete="tel"
+            placeholder="Votre numéro de téléphone"
+            value={formData.phone}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            required
+          />
+          {errors.phone && (
+            <p className="error error-msg" role="alert">
+              {errors.phone}
+            </p>
+          )}
 
-        <textarea
-          name="message"
-          placeholder="Votre message"
-          value={formData.message}
-          onChange={handleChange}
-        />
-        {errors.message && <p className="error">{errors.message}</p>}
+          <label className="label_form" htmlFor="message">
+            Ecrivez-moi vos messages
+          </label>
+          <textarea
+            id="message"
+            className={`message input_nuit ${
+              errors.message ? "input-error" : ""
+            }`}
+            name="message"
+            placeholder="Vos messages"
+            value={formData.message}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            required
+            minLength="10"
+          ></textarea>
+          {errors.message && (
+            <p className="error error-msg" role="alert">
+              {errors.message}
+            </p>
+          )}
 
-        {/* Vérification reCAPTCHA */}
-        <GoogleReCaptchaProvider onVerify={handleRecaptchaVerify} />
+          <GoogleReCaptchaProvider onVerify={handleRecaptchaVerify} />
 
-        <button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? "Envoi en cours..." : "Envoyez"}
-        </button>
-      </form>
-
-      {status && <p className="status-message">{status}</p>}
-
+          <button
+            id="buttonEnvoi"
+            className="button_denvoi input_nuit"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Envoi en cours..." : "Envoyez"}
+          </button>
+        </form>
+      </div>
+      {status && (
+        <div className={`popup-status ${status ? "show" : ""}`}>
+          <div className="popup-status-content">
+            <p>{status}</p>
+          </div>
+        </div>
+      )}
       <p className="recaptcha-message">
         Ce site est protégé par reCAPTCHA et les{" "}
         <a href="https://policies.google.com/privacy">
--- portfolio/src/app/components/GoogleReCaptchaProvider.js
+++ portfolio/src/app/components/GoogleReCaptchaProvider.js
@@ -1,42 +1,48 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 
 const GoogleReCaptchaProvider = ({ onVerify }) => {
   const [recaptchaLoaded, setRecaptchaLoaded] = useState(false);
 
   useEffect(() => {
-    if (window.grecaptcha) {
-      setRecaptchaLoaded(true);
-      return;
-    }
+    const loadReCAPTCHA = () => {
+      if (window.grecaptcha) {
+        setRecaptchaLoaded(true);
+        return;
+      }
 
-    const script = document.createElement("script");
-    script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
-    script.async = true;
-    script.defer = true;
-    script.onload = () => setRecaptchaLoaded(true);
-    document.body.appendChild(script);
+      const script = document.createElement("script");
+      script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
+      script.async = true;
+      script.defer = true;
+      script.onload = () => setRecaptchaLoaded(true);
+      document.body.appendChild(script);
+    };
+
+    loadReCAPTCHA();
   }, []);
 
-  const executeReCAPTCHA = useCallback(async () => {
-    if (!recaptchaLoaded || !window.grecaptcha) return null;
+  useEffect(() => {
+    const executeReCAPTCHA = async () => {
+      if (!recaptchaLoaded || !window.grecaptcha) return;
 
-    try {
-      const token = await window.grecaptcha.execute(
-        process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
-        { action: "submit" }
-      );
-      onVerify(token); // Stocke le token dans le state parent
-    } catch (error) {
-      console.error("Erreur lors de l'exécution de reCAPTCHA :", error);
-      onVerify(null);
+      try {
+        const token = await window.grecaptcha.execute(
+          process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+          { action: "submit" }
+        );
+        onVerify(token);
+      } catch (error) {
+        console.error("Erreur lors de l'exécution de reCAPTCHA :", error);
+        onVerify(null);
+      }
+    };
+
+    if (recaptchaLoaded) {
+      executeReCAPTCHA();
     }
   }, [recaptchaLoaded, onVerify]);
 
-  return (
-    <button onClick={executeReCAPTCHA} className="hidden">
-      Vérifier reCAPTCHA
-    </button>
-  );
+  return null;
 };
 
 export default GoogleReCaptchaProvider;
